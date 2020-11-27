ebac2df0c93988c59d7ee8bf6ebc5921a8bfa4f1
KillianM92 <killian.moutinard@ynov.com>
2020-11-27 11:15:18 +0100

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPG_JavaScript</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js"></script>
+</head>
+<?php 
+    include ("data/connect_data.php");
+?>
+<body>
+    <?php var_dump($db); ?>
+    <p> coucou </p>
+</body>
+</html>
