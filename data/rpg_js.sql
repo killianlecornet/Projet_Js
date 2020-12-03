83bc17ac36469be3f18bc7cdc9445d696f32c4c2
KillianM92 <killian.moutinard@ynov.com>
2020-12-03 18:44:55 +0100

    update des classes et stats
    ajout de colonnes dans la table "classe"

--- data/rpg_js.sql
+++ data/rpg_js.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 03 déc. 2020 à 16:57
+-- Généré le : jeu. 03 déc. 2020 à 18:44
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.5
 
@@ -29,21 +29,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `classe` (
   `id` int(11) NOT NULL COMMENT '1',
-  `pseudo` varchar(50) NOT NULL,
   `nom_classe` varchar(10) NOT NULL,
   `hp` int(11) NOT NULL,
   `atk` int(11) NOT NULL,
-  `mp` varchar(50) NOT NULL
+  `mp` varchar(50) NOT NULL,
+  `def` int(11) NOT NULL,
+  `magie` varchar(200) NOT NULL,
+  `esquive` int(11) NOT NULL,
+  `vitesse` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `classe`
 --
 
-INSERT INTO `classe` (`id`, `pseudo`, `nom_classe`, `hp`, `atk`, `mp`) VALUES
-(1, '', 'Archer', 150, 200, '300'),
-(2, '', 'Mage', 100, 250, '500'),
-(3, '', 'Guerrier', 500, 100, '150');
+INSERT INTO `classe` (`id`, `nom_classe`, `hp`, `atk`, `mp`, `def`, `magie`, `esquive`, `vitesse`) VALUES
+(1, 'Archer', 150, 200, '300', 100, 'flèche de feu, tir multiple', 50, 60),
+(2, 'Mage', 100, 250, '500', 200, 'boule de feu, éclair foudroyant', 80, 90),
+(3, 'Guerrier', 500, 100, '150', 300, 'épée de feu, épée de glace', 30, 10),
+(4, 'Centaure', 400, 150, '250', 200, 'sabot de fer, charge', 40, 80),
+(5, 'Gremlins', 75, 300, '100', 20, 'transformation démoniaque, hurlements', 100, 95);
 
 -- --------------------------------------------------------
 
@@ -61,7 +66,19 @@ CREATE TABLE `perso` (
 --
 
 INSERT INTO `perso` (`id`, `pseudo`) VALUES
-(1, 'Users1458');
+(1, 'SUZ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnage`
+--
+
+CREATE TABLE `personnage` (
+  `id_perso` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Index pour les tables déchargées
@@ -72,6 +89,22 @@ INSERT INTO `perso` (`id`, `pseudo`) VALUES
 --
 ALTER TABLE `classe`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `personnage`
+--
+ALTER TABLE `personnage`
+  ADD PRIMARY KEY (`id_perso`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `personnage`
+--
+ALTER TABLE `personnage`
+  MODIFY `id_perso` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
