343a3727b407f35781cd97457f2dce493ba2e91b
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 21:33:09 +0100

    Update connect_data.php
    Commentaire sur l'utilisation et le mode d'appel de la BDD

--- data/connect_data.php
+++ data/connect_data.php
@@ -1,3 +1,8 @@
 <?php
+
+/* Connexion à la base de données avec le système de PDO
+ en mySQL à la base de données rpg_js avec le nom 
+ d'utilisateur root et le mot de passe vide */
 $db = new PDO('mysql:host=localhost;dbname=rpg_js', 'root', '');
+
 ?>
