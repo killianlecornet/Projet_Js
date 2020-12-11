5ce141e116b12ab4780560b8616ce1341091f76a
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 22:03:36 +0100

    Update modif.php
    indentation

--- modif.php
+++ modif.php
@@ -1,11 +1,17 @@
+<?php require("inc/header.inc.php"); ?>
+
 <?php
-include ("data/connect_data.php");
+
+/*#######################################################################
+############### Connexion et requête SQL vers la BDD ####################
+#########################################################################*/
+
+require ("data/connect_data.php");
 //on modifie perso grace au set pseudo=:pseudo 
 $req = $db->prepare('UPDATE perso SET pseudo=:pseudo');
 
 // on veux que :pseudo sois egale a pseudo(récuper dans le form de index.php)
 $req->bindValue(':pseudo', $_POST['pseudo'],PDO::PARAM_STR);
-
 
 $executeIsOk = $req->execute();
 //Lorsque tout est bon cela redirige directement a classe.php
