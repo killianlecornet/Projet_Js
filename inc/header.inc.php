90550719ffff27ec5e4e8a0b3b36e529cafb8f1a
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 21:31:57 +0100

    Create header.inc.php
    Ajout d'un dossier include pour inclure le header dans tout les fichiers .php

---
+++ inc/header.inc.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src= "https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <title>RPG_JavaScript</title>
+</head>
