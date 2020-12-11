689fefbb78f42001f4f0233ac55ef4aa6ece4879
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 23:00:35 +0100

    Update classe.php
    indentation & comment

--- classe.php
+++ classe.php
@@ -4,33 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css" />
-    <title>Document</title>
+    <title>RPG_JavaScript</title>
 </head>
+
 <body>
 
-<?php 
-include ("data/connect_data.php");
-    
-$req = $db->prepare('SELECT * FROM classe');
-$executeIsOk = $req->execute();
-$liste = $req->fetchAll();
+    <?php 
+    include ("data/connect_data.php");
+        
+    $req = $db->prepare('SELECT * FROM classe');
+    $executeIsOk = $req->execute();
+    $liste = $req->fetchAll();
 
-?>
-<form class="box" action="" method="" name="login">
-<h1 class="box-title">Choisir la classe</h1>
-<?php  foreach ($liste as $liste): ?>
+    ?>
 
-<ul>
-<a href="menu.php?IDtable=<?= $liste['id'] ?>" class="btn btn-success"><?= $liste['nom_classe'] ?></a>
+    <form class="box" action="" method="" name="login">
+        <h1 class="box-title">
+            Choisir la classe
+        </h1>
 
-</ul>
-<br>
+        <?php  foreach ($liste as $liste): ?>
 
+            <ul>
+                <a href="menu.php?IDtable=<?= $liste['id'] ?>" class="btn btn-success">
+                    <?= $liste['nom_classe'] ?>
+                </a>
+            </ul>
 
-<?php endforeach ?>
-</form>
+            <br>
 
-
-
+        <?php endforeach ?>
+    </form>
 </body>
 </html>
