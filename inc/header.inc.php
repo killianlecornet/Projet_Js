48ec2f28661ed57265dcc26c0e31fbea1473e624
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 22:38:39 +0100

    Revert "Create header.inc.php"
    This reverts commit 90550719ffff27ec5e4e8a0b3b36e529cafb8f1a.

--- inc/header.inc.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src= "https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <title>RPG_JavaScript</title>
-</head>
