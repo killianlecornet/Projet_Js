dff153d386feb9669e05381af665ec5d98fe97a9
KillianM92 <killian.moutinard@ynov.com>
2020-12-11 22:41:58 +0100

    Revert "Update combat.php"
    This reverts commit 9947c31b4645668fa9f42ca6638eb1420340448c.

--- combat.php
+++ combat.php
@@ -1,92 +1,79 @@
-<?php require("inc/header.inc.php"); ?>
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
 <body>
 
-    <?php 
+<?php 
+//On apelle la connexion a la base de donnée qui est dans le dossier data
+include ("data/connect_data.php");
+// On select TOUT  dans classe qui a ['id'] demander ex: la premiere classe a 1 comme id 
+$req = $db->prepare('SELECT * FROM classe WHERE id=:num');
+// on veux que :num sois egale a IDtable
+$req->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
+// On execute
+$executeIsOk = $req->execute();
 
-        /*#######################################################################
-        ############### Connexion et requête SQL vers la BDD ####################
-        #########################################################################*/
+$liste = $req->fetch();
+// On select TOUT dans la table perso
+$req2 = $db->prepare('SELECT * FROM perso');
 
-        // On apelle la connexion a la base de donnée qui est dans le dossier data
-        require ("data/connect_data.php");
+$req2->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
 
-        // On select TOUT  dans classe qui a ['id'] demander ex: la premiere classe a 1 comme id 
-        $req = $db->prepare('SELECT * FROM classe WHERE id=:num');
+$executeIsOk2 = $req2->execute();
 
-        // on veux que :num sois egale a IDtable
-        $req->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
+$liste2 = $req2->fetch();
 
-        // On execute la requête SQL
-        $executeIsOk = $req->execute();
-
-        $liste = $req->fetch();
-
-        // On select TOUT dans la table perso
-        $req2 = $db->prepare('SELECT * FROM perso');
-
-        $req2->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
-
-        $executeIsOk2 = $req2->execute();
-
-        $liste2 = $req2->fetch();
-
-    ?>
-
-    <?php 
-
-        // On select TOUT dans la table classe avec id 
-        $req = $db->prepare('SELECT * FROM classe WHERE id=:num');
-
-        $req->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
-
-        $executeIsOk = $req->execute();
-
-        $liste = $req->fetchAll();
-    ?>
-
-    <!-- ####################################################################
-    ####################### Affichage sur la page ###########################
-    ######################################################################### -->
-
-    <!-- affichage de la barre d'état en l'appelant grâce au fichier barre_vie_perso.inc.php -->
-    <?php require ("inc/barre_vie_perso.inc.php"); ?>
-
-    <div>
-        <form class="box-fight" action="" method="" name="login">
-            <h1 class="box-title">Option de Combat</h1> <!-- choix afficher après avoir cliqué sur "combattre" -->
-            
-            <!-- grace a cette boucle on affiche tout le temps les skills quelque sois la classe -->
-            <?php  foreach ($liste as $liste): ?>
-
-                <ul style="text-align: center;padding: 0;">
-                    <a href="" class=" btn-btn-fight btn-fight">
-                        <?= $liste['skill_1'] ?>
-                    </a>  
-                    <a href="" class="btn-btn-fight btn-fight">
-                        <?= $liste['skill_2'] ?>
-                    </a>
-                    <!-- bouton des skills(sorts) -->
-                    
-                    <br>
-                    <br>
-                    <br> 
-                    
-                    <a href="menu.php?IDtable=<?= $liste['id'] ?>" class=" btn-btn-fight btn-fight">
-                        FUIR
-                    </a> 
-                    <!-- bouton fuir -->
-
-                    <div class="monster">
-                        <!--creation du monstre a afficher -->
-                    </div>
-                </ul>
-                <br>
-
-            <?php endforeach ?>
-        </form>
-
+?>
+    <!-- pour récuperer IDtable on la mis dans un hidden pour pas qu'il sois afficher -->
+    <input type="hidden"  name="IDtable" value="<?= $liste['id'] ?>">
+<div>
+   <div>
+   <img src="img/barre_vie_mana.png" style="width: 18%;"/> <!-- affichage de la barre de vie -->
+   </div>
+   <div style="position:absolute;top:-0.6%;left:10%;z-index:2;font-size:90%"><!-- positionnement de la barre d'hp -->
+    <p><?= $liste['hp'] ?>/ <?= $liste['hp'] ?></p> 
+    </div> 
+    <div style="position:absolute;top:2.5%;left:10%;z-index:2;font-size:90%"><!-- positionnement de la barre de mana -->
+    <p><?= $liste['mp'] ?>/ <?= $liste['mp'] ?></p>
     </div>
+    <div style="position:absolute;top:4.5%;left:10%;z-index:2;font-size:120%;color: tan;text-transform: uppercase;letter-spacing: 1px;"><!-- affichage de la barre du mana -->
+    <p><?= $liste2['pseudo'] ?></p>
+    </div> 
+</div>
+<?php 
+include ("data/connect_data.php");
+// On select TOUT dans la table classe avec id 
+$req = $db->prepare('SELECT * FROM classe WHERE id=:num');
+
+$req->bindValue(':num',$_GET['IDtable'],PDO::PARAM_INT);
+
+$executeIsOk = $req->execute();
+
+$liste = $req->fetchAll();
+?>
+<div>
+<form class="box-fight" action="" method="" name="login">
+<h1 class="box-title">Option de Combat</h1> <!-- choix afficher après avoir cliqué sur "combattre" -->
+<!-- grace a cette boucle on affiche tout le temps les skill quelque sois la classe -->
+<?php  foreach ($liste as $liste): ?>
+
+<ul style="text-align: center;padding: 0;">
+<a href="" class=" btn-btn-fight btn-fight"><?= $liste['skill_1'] ?></a>  <a href="" class="btn-btn-fight btn-fight"><?= $liste['skill_2'] ?></a><br><br><br> <!-- bouton des skills(sorts) -->
+<a href="menu.php?IDtable=<?= $liste['id'] ?>" class=" btn-btn-fight btn-fight">FUIR</a> <!-- bouton fuir -->
+
+ <div class="monster"></div> <!--creation du monstre a afficher -->
+</ul>
+<br>
+
+
+<?php endforeach ?>
+</form>
+</div>
     
 </body>
 </html>
